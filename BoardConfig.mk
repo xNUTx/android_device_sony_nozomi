b92985f9cf4c2dd1d1845706278ac5702a7a24ac
xNUTx <nut@fun-industries.nl>
2014-07-04 12:26:55 +0200

    Ripping out the kernel building... part 4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,7 @@ USE_CAMERA_STUB := true
 -include device/sony/qcom-common/BoardConfigCommon.mk
 
 #TARGET_KERNEL_CONFIG := opensemc_fuji_nozomi_defconfig
-TARGET_PREBUILT_KERNEL := device/sony/nozomi/prebuilt/boguskernel
+TARGET_PREBUILT_KERNEL := device/sony/nozomi/kernel
 
 # Sony-supplied broadcom wlan module
 BOARD_WLAN_BROADCOM := true
